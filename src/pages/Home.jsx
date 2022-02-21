0433cc339167315fbff3541bbaf4b6a50045996f
green68 <eric.vercheval@hotmail.fr>
2022-02-21 20:25:21 +0100

    suppress IonContent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import { IonContent } from '@ionic/react';
 
 const Home = () => {
 
   return (
-    <IonContent>
+    <>
       <p>Page Home</p>
       <p>Mettre du texte pour test affichage</p>
       <p>test pour ligne 2</p>
       <p>test pour ligne 3</p>
-    </IonContent>
+    </>
   );
 };
 
